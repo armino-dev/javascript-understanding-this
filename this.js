956b4485af8f8d492e302911be1432ca3b71119b
Armino Popp <armino.popp@gmail.com>
2020-07-02 09:37:52 +0300

    Update this.js

--- this.js
+++ this.js
@@ -1,4 +1,31 @@
-console.log(this);
+console.log(this); //window
+
+function f1() {
+  return this;
+}
+
+console.log(f1()); // window
+
+function f2() {
+  'use strict';
+  return this;
+}
+
+
+console.log(f2()); // undefined
+
+
+class Ex {
+  constructor() {
+    const proto = Object.getPrototypeOf(this);
+    console.log(Object.getOwnPropertyNames(proto));
+  }
+  one() {}
+  two() {}
+  static three() {}
+}
+
+new Ex(); // ['constructor', 'first', 'second']
 
 
 (()=>{
